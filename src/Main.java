a92d4371ed9864f7e6871c6441b13e7e335f12df
Tagir Murtazin <tr.murtazin@yandex.ru>
2023-03-19 23:05:42 +0300

    Исключить одинаковые элементы массива (каждое значение должно присутствовать в единственном экземпляре)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+/*
+Исключить одинаковые элементы массива (каждое значение должно присутствовать в единственном экземпляре)
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] arr = {2, 4, 1, 2, 1, 2, 4, 5};
+
+        int[] distinct = removeDuplicates(arr);
+        System.out.println(Arrays.toString(distinct));
+    }
+    public static int[] removeDuplicates(int[] arr) {
+        return Arrays.stream(arr).distinct().toArray();
     }
 }
